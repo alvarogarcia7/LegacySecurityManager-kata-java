09b30e5a4cb4176f258159d116c87653b84df8ef
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 16:30:28 +0200

    Refactor: whitespace

--- src/main/java/SecurityManager.java
+++ src/main/java/SecurityManager.java
@@ -6,6 +6,9 @@ import user.communication.UserNotifier;
 
 public class SecurityManager {
     public static void createUser() {
-        new CreatingUser(new UserInput(new RealReadConsole()), new UserNotifier(System.out::println)).invoke();
+        new CreatingUser(
+                    new UserInput(new RealReadConsole())
+                    , new UserNotifier(System.out::println))
+                .invoke();
     }
 }
