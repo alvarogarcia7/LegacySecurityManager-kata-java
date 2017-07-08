57058b1b812d76f30de484729138bace2144ee8f
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 14:22:26 +0200

    Defect: last refactor did not compile

--- src/main/java/SecurityManager.java
+++ src/main/java/SecurityManager.java
@@ -1,9 +1,10 @@
 
 import infrastructure.RealReadConsole;
 import user.CreatingUser;
+import user.UserNotifier;
 
 public class SecurityManager {
     public static void createUser() {
-        new CreatingUser(new RealReadConsole(), System.out::println).invoke();
+        new CreatingUser(new RealReadConsole(), new UserNotifier(System.out::println)).invoke();
     }
 }
