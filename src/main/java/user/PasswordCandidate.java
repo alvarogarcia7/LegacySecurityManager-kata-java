79f4e4f13fe833d57e93e97e3abf7f4ae35fa048
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 16:17:45 +0200

    Refactor: split the two steps validation into different functions [manual]

--- src/main/java/user/PasswordCandidate.java
+++ src/main/java/user/PasswordCandidate.java
@@ -1,9 +1,9 @@
 package user;
 
+import io.vavr.collection.Seq;
 import io.vavr.control.Either;
 import io.vavr.control.Validation;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -20,7 +20,7 @@ public class PasswordCandidate {
         return new PasswordValidator().validatePassword(firstTry, secondTry).toEither();
     }
 
-    public String encrypt() {
+    private String encrypt(String firstTry) {
         // Encrypt the password (just reverse it, should be secure)
         String encryptedPassword = new StringBuilder(firstTry).reverse().toString();
         return encryptedPassword;
@@ -40,17 +40,28 @@ public class PasswordCandidate {
 
     class PasswordValidator {
         public Validation<List<String>, EncryptedPassword> validatePassword(String firstTry, String secondTry) {
-            return Validation.narrow(validate(firstTry, secondTry)).bimap(Arrays::asList, Function.identity());
+            return Validation
+                    .combine(validateSame(firstTry, secondTry), validateLength(firstTry))
+                    .ap(this::buildPassword)
+                    .bimap(Seq::asJava, Function.identity());
         }
 
-        private Validation<String, EncryptedPassword> validate(String firstTry, String secondTry) {
+        private Validation<String, String> validateLength(String password) {
+            if (password.length() < 8) {
+                return Validation.invalid("Password must be at least 8 characters in length");
+            }
+            return Validation.valid(password);
+        }
+
+        private Validation<String, String> validateSame(String firstTry, String secondTry) {
             if (!firstTry.equals(secondTry)) {
                 return Validation.invalid("The passwords don't match");
             }
-            if (firstTry.length() < 8) {
-                return Validation.invalid("Password must be at least 8 characters in length");
-            }
-            return Validation.valid(new EncryptedPassword(encrypt()));
+            return Validation.valid(firstTry);
+        }
+
+        private EncryptedPassword buildPassword(String s, String _) {
+            return new EncryptedPassword(encrypt(s));
         }
 
     }
