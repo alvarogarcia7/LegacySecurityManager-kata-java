729d8909687fbc813cb1c0b2d45232876c8a9361
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 16:05:54 +0200

    Refactor: push the behaviour down to a PasswordValidator using vavr.io's Validation [manual]

--- src/main/java/user/PasswordCandidate.java
+++ src/main/java/user/PasswordCandidate.java
@@ -1,9 +1,11 @@
 package user;
 
 import io.vavr.control.Either;
+import io.vavr.control.Validation;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class PasswordCandidate {
     private final String firstTry;
@@ -15,20 +17,7 @@ public class PasswordCandidate {
     }
 
     public Either<List<String>, EncryptedPassword> isValid() {
-        List<String> messages = new ArrayList<>();
-        if (!firstTry.equals(secondTry)) {
-            messages.add("The passwords don't match");
-        }
-
-        if (firstTry.length() < 8) {
-            messages.add("Password must be at least 8 characters in length");
-        }
-
-        if(messages.isEmpty()){
-            return Either.right(new EncryptedPassword(encrypt()));
-        } else {
-            return Either.left(messages);
-        }
+        return new PasswordValidator().validatePassword(firstTry, secondTry).toEither();
     }
 
     public String encrypt() {
@@ -47,5 +36,22 @@ public class PasswordCandidate {
         public String value() {
             return value;
         }
+    }
+
+    class PasswordValidator {
+        public Validation<List<String>, EncryptedPassword> validatePassword(String firstTry, String secondTry) {
+            return Validation.narrow(validate(firstTry, secondTry)).bimap(Arrays::asList, Function.identity());
+        }
+
+        private Validation<String, EncryptedPassword> validate(String firstTry, String secondTry) {
+            if (!firstTry.equals(secondTry)) {
+                return Validation.invalid("The passwords don't match");
+            }
+            if (firstTry.length() < 8) {
+                return Validation.invalid("Password must be at least 8 characters in length");
+            }
+            return Validation.valid(new EncryptedPassword(encrypt()));
+        }
+
     }
 }
