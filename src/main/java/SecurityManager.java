5dc9424ff5fb94708716cd50e1803ec8d45594f3
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 13:36:40 +0200

    Refactor: extract anonymous ReadConsole to an upper-level one [automatic]

--- src/main/java/SecurityManager.java
+++ src/main/java/SecurityManager.java
@@ -1,27 +1,9 @@
 
-import infrastructure.ReadConsole;
-import infrastructure.WriteConsole;
+import infrastructure.RealReadConsole;
 import user.CreatingUser;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SecurityManager {
     public static void createUser() {
-        new CreatingUser(new ReadConsole() {
-            public final BufferedReader buffer;
-
-            {
-                buffer = new BufferedReader(new InputStreamReader(System.in));
-
-            }
-
-            @Override
-            public String readLine() throws IOException {
-                return buffer.readLine();
-            }
-        }, System.out::println).invoke();
+        new CreatingUser(new RealReadConsole(), System.out::println).invoke();
     }
-
 }
---
+++ src/main/java/infrastructure/RealReadConsole.java
@@ -0,0 +1,19 @@
+package infrastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RealReadConsole implements ReadConsole {
+    public final BufferedReader buffer;
+
+    {
+        buffer = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+    @Override
+    public String readLine() throws IOException {
+        return buffer.readLine();
+    }
+}
