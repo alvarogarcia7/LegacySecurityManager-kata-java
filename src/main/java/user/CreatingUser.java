8fba9d521b7d6e5ff0e825e0d318b577ea7fdbae
Alvaro Garcia <derecerca@gmail.com>
2017-07-08 14:18:13 +0200

    Refactor: apply last refactoring to the rest [manual]

--- src/main/java/user/CreatingUser.java
+++ src/main/java/user/CreatingUser.java
@@ -20,12 +20,9 @@ public class CreatingUser {
         UserData userData = null;
         try {
             String username = userNotifier.prompt("Enter a username", inputReadConsole);
-            userNotifier.inform("Enter your full name");
-            String fullName = inputReadConsole.readLine();
-            userNotifier.inform("Enter your password");
-            String password = inputReadConsole.readLine();
-            userNotifier.inform("Re-enter your password");
-            String confirmPassword = inputReadConsole.readLine();
+            String fullName = userNotifier.prompt("Enter your full name", inputReadConsole);
+            String password = userNotifier.prompt("Enter your password", inputReadConsole);
+            String confirmPassword = userNotifier.prompt("Re-enter your password", inputReadConsole);
             userData = new UserData(username, fullName, new PasswordCandidate(password, confirmPassword));
         } catch (IOException e) {
             e.printStackTrace();
